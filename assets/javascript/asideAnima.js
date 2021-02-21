189ae03d1fdd21ba284ad29b3f2c49b7773c9ec4
matheus souza <ruivorobado@gmail.com>
2021-02-21 14:13:03 -0300

    adicionando correcao a aside

--- assets/javascript/asideAnima.js
+++ assets/javascript/asideAnima.js
@@ -4,9 +4,11 @@ function ativaAside(){
   if(aside.style.width == '240px'){ 
     return (
       aside.style.width = '0',
-     main.style.marginLeft = '0'
+     main.style.marginLeft = '0',
+     main.style.width = '100%'
      )
   }
   aside.style.width = '240px'
+  main.style.width = 'calc(100% - 240px)'
   main.style.marginLeft = '240px'
 }
