a7c5c500c20aa7080eb520866b591cdb43b51311
matheus souza <ruivorobado@gmail.com>
2021-02-21 14:09:14 -0300

    finalizacao da aside responsica

--- assets/javascript/asideAnima.js
+++ assets/javascript/asideAnima.js
@@ -1,5 +1,12 @@
 function ativaAside(){
   const aside = document.querySelector('aside#aside')
-  if(aside.style.width == '240px') return aside.style.width = '0'
+  const main = document.querySelector('main#main')
+  if(aside.style.width == '240px'){ 
+    return (
+      aside.style.width = '0',
+     main.style.marginLeft = '0'
+     )
+  }
   aside.style.width = '240px'
+  main.style.marginLeft = '240px'
 }
